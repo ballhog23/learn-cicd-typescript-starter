7f53d90f9aaac572e29197fc3b7e8b9503372a83
caleb <calebpirkle2@gmail.com>
2026-03-01 23:52:21 -0600

    cloud run cd

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import pluginSecurity from "eslint-plugin-security";
-
+import html from '@html-eslint/eslint-plugin';
 
 export default defineConfig([
   {
@@ -12,6 +12,16 @@ export default defineConfig([
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node }
   },
+  {
+    files: ["**/*.html"],
+    rules: {
+      ...html.configs.recommended.rules,
+      "html/require-img-alt": "error",
+      "html/no-duplicate-attrs": "error"
+    }
+  },
   tseslint.configs.recommended,
-  pluginSecurity.configs.recommended
+  pluginSecurity.configs.recommended,
+  html.configs.recommended
 ]);
+
